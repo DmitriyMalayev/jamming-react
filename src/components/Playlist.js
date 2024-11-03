429aef85b831ae533f4a67a5a6a59464b95c5825
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-11-02 23:32:02 -0400

    fixed button

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -10,11 +10,25 @@ const Playlist = ({ onNameChange, playlistTracks, onRemove, onSave }) => {
     [onNameChange]
   );
 
+  const checkPlaylistTracks = () => {
+    if (playlistTracks.length === 0) {
+      return true;
+    }
+    return false;
+  };
+
+  console.log(playlistTracks);
   return (
     <div className={styles.PlayList}>
       <input onChange={handleNameChange} defaultValue={"New Playlist"} />
       <TrackList tracks={playlistTracks} isRemoval={true} onRemove={onRemove} />
-      <button className={styles.PlayListSave} onClick={onSave}>
+      <button
+        className={
+          checkPlaylistTracks() ? styles.PlayListDisabled : styles.PlayListSave
+        }
+        disabled={checkPlaylistTracks()}
+        onClick={onSave}
+      >
         SAVE TO SPOTIFY
       </button>
     </div>
